f571dac2cd484a0125057778a86e62761d5a0e26
gego144 <56304474+gego144@users.noreply.github.com>
2021-05-23 15:51:52 -0300

    Rest of game

---
+++ Assets/FenceChecker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FenceChecker : MonoBehaviour
+{
+    void OnCollisionEnter (Collision collision)
+    {
+        if (collision.collider.tag == "Fence")
+        {
+            Debug.Log("Working bruv");
+        }
+    }
+}
---
+++ Assets/hunterFootsteps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hunterFootsteps : MonoBehaviour
+
+{
+    public AudioClip[] footsounds;
+    private AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    public void footstep(){
+        AudioClip clip = footsounds[UnityEngine.Random.Range(0, footsounds.Length)];
+        sound.PlayOneShot(clip, 0.7F);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
